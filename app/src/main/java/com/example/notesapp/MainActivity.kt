3df39801d196889fd83f71588284d96873e89e5c
choboribi <jacksonahyde@gmail.com>
2022-04-08 12:45:41 -0500

    forgot to save smh

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -9,6 +9,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        
     }
 }

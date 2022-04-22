38abc1484b54df86cabc07238a417dc55a2180db
Prince <princepreet177@gmail.com>
2022-04-22 15:43:57 -0500

    add qr scanner

--- app/src/main/java/com/example/notesapp/NotesActivity.kt
+++ app/src/main/java/com/example/notesapp/NotesActivity.kt
@@ -7,11 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.example.notesapp.databinding.ActivityNotesBinding
 import entities.Note
+import io.github.g00fy2.quickie.QRResult
+import io.github.g00fy2.quickie.ScanQRCode
 
 class NotesActivity : AppCompatActivity() {
     private lateinit var inNotes: MutableList<Note>
     private lateinit var binding: ActivityNotesBinding
     private var parentSetId: Long = -1
+    val scanQrCode = registerForActivityResult(ScanQRCode(), ::handleResult)
+
+    fun handleResult(result: QRResult) {
+        var x=1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
@@ -29,6 +36,15 @@ class NotesActivity : AppCompatActivity() {
 
         binding.addNoteButton.setOnClickListener{
             (binding.notesRecyclerView.adapter as NotesAdapter).addItem(Note("New", "", parentSetId))
+                binding.notesRecyclerView.smoothScrollToPosition((binding.notesRecyclerView.adapter as NotesAdapter).itemCount - 1)
+        }
+
+        binding.scanqrcode.setOnClickListener{
+            scanQrCode.launch(null)
+        }
+
+        fun handleResult(result: QRResult) {
+            (binding.notesRecyclerView.adapter as NotesAdapter).addItem(Note("Imported Note", result.toString(), parentSetId))
                 binding.notesRecyclerView.smoothScrollToPosition((binding.notesRecyclerView.adapter as NotesAdapter).itemCount - 1)
         }
     }
